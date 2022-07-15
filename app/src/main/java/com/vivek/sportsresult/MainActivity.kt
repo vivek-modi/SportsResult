8a2b6f3fac401df77d023776c0a10a892c7db006
Vivek Modi <vivekmodi456@gmail.com>
2022-07-15 16:12:01 +0100

    added koin dependency with initial setup of koin

--- app/src/main/java/com/vivek/sportsresult/MainActivity.kt
+++ app/src/main/java/com/vivek/sportsresult/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.vivek.sportsresult.ui.theme.SportsResultTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
---
+++ app/src/main/java/com/vivek/sportsresult/ResultApplication.kt
@@ -0,0 +1,23 @@
+package com.vivek.sportsresult
+
+import android.app.Application
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class ResultApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initializeDependencyInjection()
+    }
+
+    private fun initializeDependencyInjection() {
+        startKoin {
+            androidLogger(Level.ERROR)
+            modules(
+                listOf()
+            )
+        }
+    }
+}
