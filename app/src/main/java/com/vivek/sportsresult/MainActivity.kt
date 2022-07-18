fe3b97cb361d1a5f91ca636df14918ca38b35f6b
Vivek Modi <vivekmodi456@gmail.com>
2022-07-18 15:34:26 +0100

    added live data dependency and check connection

--- app/src/main/java/com/vivek/sportsresult/MainActivity.kt
+++ app/src/main/java/com/vivek/sportsresult/MainActivity.kt
@@ -15,54 +15,69 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.vivek.sportsresult.connection.NetworkConnection
 import com.vivek.sportsresult.ui.theme.SportsResultTheme
 import com.vivek.sportsresult.ui.theme.getBackgroundColor
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var networkConnection: NetworkConnection
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        networkConnection = NetworkConnection(application)
         setContent {
             SportsResultTheme {
-                SetupView()
+                SetupConnectionView()
             }
         }
     }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SetupView() {
-    Scaffold(topBar = {
-        TopAppBar(
-            title = { Text(text = stringResource(id = R.string.app_name)) },
-            backgroundColor = getBackgroundColor(),
-            elevation = 0.dp
-        )
-    }, content = { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(getBackgroundColor())
-                .padding(padding),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = stringResource(id = R.string.get_result))
-            }
+    @Composable
+    fun SetupConnectionView() {
+        val isConnected = networkConnection.observeAsState()
+        if (isConnected.value == true) {
+            SetupView()
+        } else {
+            println("No Connected with data >>>>>>>>>")
         }
-    })
-}
+    }
 
-@Preview("Light Theme")
-@Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-fun ShowViewInPreview() {
-    SetupView()
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SetupView() {
+        Scaffold(topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name)) },
+                backgroundColor = getBackgroundColor(),
+                elevation = 0.dp
+            )
+        }, content = { padding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(getBackgroundColor())
+                    .padding(padding),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = stringResource(id = R.string.get_result))
+                }
+            }
+        })
+    }
+
+    @Preview("Light Theme")
+    @Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+    @Composable
+    fun ShowViewInPreview() {
+        SetupView()
+    }
 }
