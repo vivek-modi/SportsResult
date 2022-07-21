788b09f52daadc8ccd18b6b9df01eaa541b307f9
Vivek Modi <vivekmodi456@gmail.com>
2022-07-21 16:47:13 +0100

    added logic to filter nearest date

--- app/src/main/java/com/vivek/sportsresult/data/ResultFetchState.kt
+++ app/src/main/java/com/vivek/sportsresult/data/ResultFetchState.kt
@@ -1,9 +1,9 @@
 package com.vivek.sportsresult.data
 
-import com.vivek.sportsresult.data.models.SportResultResponse
+import com.vivek.sportsresult.data.models.NearestResult
 
 sealed class ResultFetchState {
-    data class OnSuccess(val sportResultResponse: SportResultResponse) : ResultFetchState()
+    data class OnSuccess(val nearestResult: List<NearestResult>) : ResultFetchState()
     object OnEmpty : ResultFetchState()
     data class OnError(val response: String?) : ResultFetchState()
     object IsLoading : ResultFetchState()
--- app/src/main/java/com/vivek/sportsresult/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/vivek/sportsresult/viewmodel/MainActivityViewModel.kt
@@ -2,15 +2,33 @@ package com.vivek.sportsresult.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.vivek.sportsresult.data.repository.ResultRepository
 import com.vivek.sportsresult.data.ResultFetchState
+import com.vivek.sportsresult.data.models.NearestResult
+import com.vivek.sportsresult.data.models.SportResultResponse
+import com.vivek.sportsresult.data.repository.ResultRepository
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import java.lang.Long.MAX_VALUE
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import java.time.temporal.Temporal
+import java.util.*
+import kotlin.math.abs
 
 class MainActivityViewModel(private val resultRepository: ResultRepository) : ViewModel() {
 
+    companion object {
+        private const val DATE_PATTERN = "MMM d, yyyy h:mm:ss a"
+    }
+
     val stateResultFetchState = MutableStateFlow<ResultFetchState>(ResultFetchState.OnEmpty)
+    private val zoneId = ZoneId.systemDefault()
+    private val today = LocalDate.now(zoneId)
+    private val startOfToday: ZonedDateTime = today.atStartOfDay(zoneId)
 
     fun getSportResult() {
         viewModelScope.launch {
@@ -20,7 +38,7 @@ class MainActivityViewModel(private val resultRepository: ResultRepository) : Vi
             result.handleResult(
                 onSuccess = { response ->
                     if (response != null) {
-                        stateResultFetchState.value = ResultFetchState.OnSuccess(response)
+                        stateResultFetchState.value = ResultFetchState.OnSuccess(getNearestDateFilter(response))
                     } else {
                         stateResultFetchState.value = ResultFetchState.OnEmpty
                     }
@@ -32,4 +50,79 @@ class MainActivityViewModel(private val resultRepository: ResultRepository) : Vi
             )
         }
     }
+
+    private fun getNearestDateFilter(response: SportResultResponse): List<NearestResult> {
+        val fOneList = getFOneNearestResult(response)
+        val nbaList = getNbaNearestResult(response)
+        val tennisList = getTennisNearestResult(response)
+        val mergeList = concatenate(fOneList, nbaList, tennisList)
+        return mergeList.sortedBy { it.day }.reversed()
+    }
+
+    private fun getFOneNearestResult(response: SportResultResponse): List<NearestResult> {
+        val fOneResultMutableList = mutableListOf<NearestResult>()
+        response.f1Results?.forEach { fOneResult ->
+            val localDateFormat = fOneResult.publicationDate?.let { publicationDate ->
+                getFormatDate(publicationDate)
+            }
+            localDateFormat?.let { localDate ->
+                NearestResult(localDate, fOneResult.getFOneResultData)
+            }?.let { item ->
+                fOneResultMutableList.add(item)
+            }
+        }
+        return getClosestDays(startOfToday, fOneResultMutableList)
+    }
+
+    private fun getNbaNearestResult(response: SportResultResponse): List<NearestResult> {
+        val nbaResultMutableList = mutableListOf<NearestResult>()
+        response.nbaResults?.forEach { nbaResult ->
+            val localDateFormat = nbaResult.publicationDate?.let { publicationDate ->
+                getFormatDate(publicationDate)
+            }
+            localDateFormat?.let { localDate ->
+                NearestResult(localDate, nbaResult.getNbaResultData)
+            }?.let { item ->
+                nbaResultMutableList.add(item)
+            }
+        }
+        return getClosestDays(startOfToday, nbaResultMutableList)
+    }
+
+    private fun getTennisNearestResult(response: SportResultResponse): List<NearestResult> {
+        val tennisMutableList = mutableListOf<NearestResult>()
+        response.Tennis?.forEach { tennis ->
+            val localDateFormat = tennis.publicationDate?.let { publicationDate ->
+                getFormatDate(publicationDate)
+            }
+            localDateFormat?.let { localDate ->
+                NearestResult(localDate, tennis.getTennisData)
+            }?.let { item ->
+                tennisMutableList.add(item)
+            }
+        }
+        return getClosestDays(startOfToday, tennisMutableList)
+    }
+
+    private fun getFormatDate(date: String): ZonedDateTime {
+        val dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_PATTERN)
+            .withZone(ZoneId.systemDefault())
+            .withLocale(Locale.ENGLISH)
+        return ZonedDateTime.parse(date, dateTimeFormatter)
+    }
+
+    private fun getClosestDays(toDate: Temporal, results: List<NearestResult>): List<NearestResult> {
+        var minimumDayCount = MAX_VALUE
+        results.forEach {
+            minimumDayCount = minOf(minimumDayCount, abs(ChronoUnit.DAYS.between(toDate, it.day)))
+        }
+        return results.filter { abs(ChronoUnit.DAYS.between(toDate, it.day)) == minimumDayCount }
+    }
+
+    private fun <T> concatenate(vararg lists: List<T>): List<T> {
+        return listOf(*lists).flatten()
+    }
+
 }
+
+
