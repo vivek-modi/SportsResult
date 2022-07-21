420335fa2be38b394ecd86cfb4d2ecadc5656ce6
Vivek Modi <vivekmodi456@gmail.com>
2022-07-21 21:41:17 +0100

    added loading spinner in loading screen

--- app/src/main/java/com/vivek/sportsresult/ui/screen/LoadingScreen.kt
+++ app/src/main/java/com/vivek/sportsresult/ui/screen/LoadingScreen.kt
@@ -4,10 +4,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.vivek.sportsresult.R
 import com.vivek.sportsresult.ui.theme.getBackgroundColor
 
 @Composable
@@ -19,6 +26,12 @@ fun LoadingFunction() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        CircularProgressIndicator()
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
+        LottieAnimation(
+            composition,
+            iterations = LottieConstants.IterateForever
+        )
+
+        Text(text = stringResource(id = R.string.please_wait))
     }
 }
