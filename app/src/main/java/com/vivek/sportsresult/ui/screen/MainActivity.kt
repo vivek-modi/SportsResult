efc26af6b88ed2c34ec7328187812f2a68cb6fb4
Vivek Modi <vivekmodi456@gmail.com>
2022-07-22 11:36:18 +0100

    remove unused stuff

--- app/src/main/java/com/vivek/sportsresult/ui/screen/MainActivity.kt
+++ app/src/main/java/com/vivek/sportsresult/ui/screen/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -75,7 +74,6 @@ fun NoInternetView() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SetupMainActivityView(
     viewModel: MainActivityViewModel = koinViewModel(),
